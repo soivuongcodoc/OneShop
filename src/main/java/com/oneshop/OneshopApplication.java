36ef3ed58b0648d0c68e77f2c946cb578ab80e0f
soivuongcodoc <nguyendinhhuylop91@gmail.com>
2025-10-20 20:30:29 +0700

    Initial commit: OneShop base structure

---
+++ src/main/java/com/oneshop/OneshopApplication.java
@@ -0,0 +1,13 @@
+package com.oneshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneshopApplication.class, args);
+	}
+
+}
